351f8a7e2843db114d3f803226bb587cae5e1580
Nathaniel Collier <drxs3v3n@gmail.com>
2016-10-11 17:18:10 -0500

    Added in an NUnit test project, and then tested the property change notification from a view model

---
+++ Tests/Test.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+namespace Tests
+{
+	[TestFixture()]
+	public class Test
+	{
+
+		class Model : ViewModelBase.ViewModelBase
+		{
+			public string Prop1
+			{
+				get { return base.GetValue(() => this.Prop1); }
+				set { base.SetValue(() => this.Prop1, value); }
+			}
+		}
+
+		[Test()]
+		public void TestPropertyChangedNotification()
+		{
+			var model = new Model();
+
+			bool changeDetected = false;
+
+			model.PropertyChanged += (s, args) =>
+			{
+				if (string.Equals(args.PropertyName, "Prop1"))
+				{
+					changeDetected = true;
+				}
+			};
+
+			System.Threading.Tasks.Task.Run(() =>
+			{
+				// need to introduce a small delay into this so that property change can fire
+				model.Prop1 = "Hello World";
+			}).Wait(new TimeSpan(0,0,30));
+
+			Assert.IsTrue(changeDetected);
+		}
+	}
+}
